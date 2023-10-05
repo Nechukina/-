767765a5645fb27729aeb2e2e1985b00f876c754
Ksenia <ksenechka.93@gmail.com>
2023-10-05 13:46:44 +0300

    add: modal

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const header = document.querySelector('.header');
 const buttonYellow = document.querySelector('.btn-warning');
 const buttonGreen = document.querySelector('.btn-success');
+const modal = document.querySelector('#exampleModal');
 
 const onButtonYellowClick = () => {
     header.classList.toggle('hidden');
     }
 
 buttonYellow.addEventListener('click', onButtonYellowClick);
+
+$(document).ready(function($) {
+	$('.popup-close').click(function() {
+		$(this).parents('.popup-fade').fadeOut();
+		return false;
+	});        
+ 
+	$(document).keydown(function(e) {
+		if (e.keyCode === 27) {
+			e.stopPropagation();
+			$('.popup-fade').fadeOut();
+		}
+	});
+	
+	$('.popup-fade').click(function(e) {
+		if ($(e.target).closest('.popup').length == 0) {
+			$(this).fadeOut();					
+		}
+	});	
+});
 
 $(document).ready(function () {
     $(buttonGreen).click(function() {
