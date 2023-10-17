ad377aed750bee03f94e188ffc4b47e62f010d57
Ksenia <ksenechka.93@gmail.com>
2023-10-17 22:12:10 +0300

    add: close popup by clicking on overlay

--- part1/index.js
+++ part1/index.js
@@ -2,8 +2,9 @@ const header = document.querySelector('.header');
 const buttonYellow = document.querySelector('.btn-warning');
 const buttonGreen = document.querySelector('.btn-success');
 const box2 = document.querySelector('.box-2');
-const modal = document.querySelector('.popup-fade');
+const modal = document.querySelector('.popup-wrapper');
 const closeButton = document.querySelector('.popup-close');
+const overlay = document.querySelector('.overlay');
 
 
 const onButtonYellowClick = () => {
@@ -26,21 +27,31 @@ const toggleBoxes = () => {
   }
 };
 
-const onCloseBtnClick = () => {
+const hideModal = () => {
   modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const onCloseBtnClick = () => {
+  hideModal();
   closeButton.removeEventListener('click', onCloseBtnClick);
 };
 
 const onEscPress = (evt) => {
   if (evt.keyCode === 27) {
-	modal.classList.add('hidden');
-	document.removeEventListener('keydown', onEscPress);
+	  hideModal();
+	  document.removeEventListener('keydown', onEscPress);
   }
-
 };
+
+const onOverlayClick = (evt) => {
+  evt.stopPropagation();
+  hideModal();
+  overlay.removeEventListener('click', onOverlayClick);
+}
 
 buttonYellow.addEventListener('click', onButtonYellowClick);
 buttonGreen.addEventListener('click', onButtonGreenClick);
 closeButton.addEventListener('click', onCloseBtnClick);
 document.addEventListener('keydown', onEscPress);
-
+overlay.addEventListener('click', onOverlayClick);
